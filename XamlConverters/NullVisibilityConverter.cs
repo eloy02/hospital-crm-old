0a64ea1173e1aae74639ecc44f77e2a069f216ad
Trefilov Ilya <tis.str02@gmail.com>
2018-10-26 09:10:49 +0500

    много фиксов

---
+++ XamlConverters/NullVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace XamlConverters
+{
+    public class NullVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string val)
+            {
+                return string.IsNullOrEmpty(val) ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return value == null ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
